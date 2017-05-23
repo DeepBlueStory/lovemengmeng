ad2bbc2b814ef46f6ba20a29be6ffac127d688c0
Blue Li <bluel@bluel-m01.vmware.com>
2017-05-23 17:50:58 +0800

    new page design

---
+++ assets/js/custom/slider/custom.js
@@ -0,0 +1,41 @@
+﻿(function ($) {
+
+    "use strict";
+
+    /* ==========================================================================
+                            check document is ready, then
+   ========================================================================== */
+
+    $(document).ready(function () {
+        
+            var $boyCounter = $(".boycounter");
+
+            if ($boyCounter.length) {
+
+                $.backstretch([
+                             '../assets/images/slider/1.jpg'
+                             , '../assets/images/slider/2.jpg'
+                             , '../assets/images/slider/3.jpg'
+                ],
+                         { duration: 4000, fade: 1000 })
+                ;
+
+                //enter the last menstrual period date using the date format  year, month, day
+                $boyCounter.tictic({
+                    date: {
+                        year: 2017,
+                        month: 5,
+                        day: 1
+                    },
+                    charts: {
+                        disableAnimation: false,
+                        darkerColor: '#FF8A65',
+                        lighterColor: '#FFAB91'
+                    }
+                });
+
+            }
+    });
+
+})(window.jQuery);
+
